aca679a4450b0b63decb74cae60b1259158af570
mdc0046-hash <mdc0046@alu.medac.es>
2026-04-30 13:50:03 +0200

    Añadiendo Ventana_Principal (JFrame)

---
+++ Ventana_Principal.java
@@ -0,0 +1,48 @@
+package Proyecto_Integral;
+
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Ventana_Principal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana_Principal frame = new Ventana_Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ventana_Principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		JLabel label = new JLabel("Prueba");
+		contentPane.add(label);
+		setTitle("Proyecto_Principal");
+		setIconImage(new ImageIcon("icono.png").getImage());
+
+	}
+
+}
